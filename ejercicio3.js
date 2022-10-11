f50f22c09641a8347c72744b35b9eb13699fb4a8
camilohm1 <cahernandez3510@misena.edu.co>
2022-10-11 08:36:57 -0500

    ejercicio3

---
+++ ejercicio3.js
@@ -0,0 +1,9 @@
+addEventListener("DOMContentLoaded", ()=>{
+//edad de una madre cuando dio a luz a uno de sus hijos
+let Madre=Number(prompt("ingrese la edad actual de la madre "))
+let hijo=Number(prompt("ingrese la edad actual del hijo "))
+//proceso
+let dioaL= Madre-hijo
+//salida o entrega de datos
+console.log("cuando dio a luz tenia la edad de: ",dioaL)
+})
