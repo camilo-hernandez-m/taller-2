944c1285b6baa074d60ea3c4486561acd7d2c0f2
camilohm1 <cahernandez3510@misena.edu.co>
2022-10-11 08:37:30 -0500

    ejercicio7

---
+++ ejercicio7.js
@@ -0,0 +1,13 @@
+addEventListener("DOMContentLoaded", ()=>{
+    // Definir cuantos kg transporta el camion
+    // transporta 25cajas
+    //cada una pesa 728kg
+    //Datos de entrada
+    let caj=25
+    let pesokg=728
+    //procesos
+    let kg=(caj*pesokg)
+    
+    //salida o entrega de resultados
+    console.log("peso total de kg que tranporta es: ",kg)
+})
