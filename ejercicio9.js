033de096cc4d02198dd621ff6cfddb5b67836473
camilohm1 <cahernandez3510@misena.edu.co>
2022-10-11 08:37:47 -0500

    ejercicio9

---
+++ ejercicio9.js
@@ -0,0 +1,13 @@
+addEventListener("DOMContentLoaded", ()=>{
+    //determinar caunto puede cobrar la brocha gorda por trabajos
+    // se cobra por m2
+    //realizar algoritmo que permita generar diferentes presupuestos para cada cliente
+    //Datos de entrada
+    let m2=Number(prompt("Ingrese los metros cuadrados que desea pintar:5"))
+    
+    //procesos
+    let vlr=(9800*m2)
+    
+    //Salida o entrega de resultados
+    console.log(" valor para pintar su area deseada es de: ",vlr)
+})
